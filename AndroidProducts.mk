b700c52a9cecb92d68dc7f51628e6296f0ce394e
Willyabel <monterowilly1@gmail.com>
2023-11-15 08:59:43 -0400

    Sweet: BringUP Evox

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_sweet.mk
+    $(LOCAL_DIR)/evolution_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_sweet-user \
-    derp_sweet-userdebug \
-    derp_sweet-eng
+    evolution_sweet-user \
+    evolution_sweet-userdebug \
+    evolution_sweet-eng
--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,13 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Derpfest stuff.
-$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+# Inherit some common sweet stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-# Derp
-DERP_BUILDTYPE := Official
 
-PRODUCT_NAME := derp_sweet
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
